52c4819dfaa26bb5c7c7726d9cc5110083d76c67
Kim <kim.winters82@gmail.com>
2020-11-30 14:47:00 +0000

    added notes and return 0 if empty

--- Main.java
+++ Main.java
@@ -1,34 +1,36 @@
 import java.util.*;
+import java.util.LinkedList;
 
 public class Solution {
   public static void main(String[] args) {
     Scanner myscanner = new Scanner(System.in);
     int num = Integer.parseInt(myscanner.nextLine());
-    Link[] array = new Link [num];
-    for (int i = 0; i<num; i++){
+    Link[] array = new Link [num]; //creates array of links
+    for (int i = 0; i<num; i++){ //adds values to each element in array
       array[i] = new Link(Integer.parseInt(myscanner.nextLine()));
     }
     while(myscanner.hasNext()){
       int select = myscanner.nextInt();
       int previous = myscanner.nextInt();
       int next = myscanner.nextInt();
-      if(previous!=-1){
+      if(previous!=-1){ //sets connection to previous and next element
         array[select].previous = array[previous];
       }
       if (next !=-1){
         array[select].next=array[next];
       }
     }
-    LinkedList mylist = new LinkedList();
+    LinkedList mylist = new LinkedList(); //creates linkedlist object ->mylist
     if(num>0){
-      mylist.first = array[0];
-      mylist.last = array[num-1];
+      mylist.first = array[0];//sets start pointer to the first element in array
+      mylist.last = array[num-1]; //sets end pointer to the final element in the array
     }
     System.out.println(search(mylist));
   }
 
   public static int search(Linkedlist a){
-    return 0; //if list empty or broken
+    if (array.isEmpty()){
+      return 0; }//if list empty or broken
     //else return largest element in the linkedlist
   }
 }
